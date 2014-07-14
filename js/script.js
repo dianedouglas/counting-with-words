ceafcc60577d6af55ada5e60a31cf622f854c970
dianedouglas & Lee Kebler <student@epicodus.com>
2014-07-14 15:33:04 -0700

    Expanded our initial function to include all the numbers from 0 to 999.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,43 @@
-var numericalValue = function(inputNumber){
+var firstNumberSection = function(inputNumber){
   inputNumber = inputNumber.toString().split("");
   var singleDigitWords = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
   var secondDigitWords = ["teen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" ]
   var teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
   var outputWord = "";
 
-  if (inputNumber.length === 1){
-    outputWord = singleDigitWords[inputNumber[0]];
-  } else if (inputNumber.length === 2){
-
-    if (inputNumber[1] === "0" && inputNumber[0] !== "1"){ //10 happens here
-      outputWord = secondDigitWords[inputNumber[0]-1];
-    } else if (inputNumber[0] === "1"){
-      outputWord = teens[inputNumber[1]];
-    }
-    else {
-      outputWord = secondDigitWords[inputNumber[0]-1] + " " + singleDigitWords[inputNumber[1]];
+  if (inputNumber.length === 3){
+    if((inputNumber[1] === "0") && (inputNumber[2] === "0")){
+      return outputWord = singleDigitWords[inputNumber[0]] + " hundred";
+    } else{
+      outputWord = singleDigitWords[inputNumber[0]] + " hundred and ";
+      inputNumber.splice(0, 1);
     }
   }
 
 
+  if (inputNumber.length === 1){
+    outputWord += singleDigitWords[inputNumber[0]];
+  } else if (inputNumber.length === 2){
+
+    if (inputNumber[1] === "0" && inputNumber[0] !== "1"){ //10 happens here
+      outputWord += secondDigitWords[inputNumber[0]-1];
+    } else if (inputNumber[0] === "1"){
+      outputWord += teens[inputNumber[1]];
+    }
+    else {
+      outputWord += secondDigitWords[inputNumber[0]-1] + " " + singleDigitWords[inputNumber[1]];
+    }
+  }
   return outputWord;
 }
+
+// nine hundred ninety nine billion
+// nine hundred ninety nine million
+// nine hundred ninety nine thousand
+// nine hundred ninety nine
+
+// nine hundred ninety nine billion
+// nine hundred and nine million
+// nine hundred ninety nine thousand
+// nine hundred ninety nine
+
--- specs/specs.js
+++ specs/specs.js
@@ -1,21 +1,29 @@
-describe("numericalValue", function(){
+describe("firstNumberSection", function(){
   it("takes a single digit numerical value and returns its word version",function(){
-    numericalValue(1).should.equal("one");
+    firstNumberSection(1).should.equal("one");
   });
   it("takes a number in the 20's and returns its word version.", function(){
-    numericalValue(25).should.equal("twenty five");
+    firstNumberSection(25).should.equal("twenty five");
   });
   it("takes a number in the 30's and returns its word version.", function(){
-    numericalValue(39).should.equal("thirty nine");
+    firstNumberSection(39).should.equal("thirty nine");
   });
   it("takes a multiple of 10 and returns its word version without a zero on the end.", function(){
-    numericalValue(40).should.equal("forty");
+    firstNumberSection(40).should.equal("forty");
   });
   it("takes a double digit number between 10 and 19 and outputs its word value", function(){
-    numericalValue(15).should.equal("fifteen");
+    firstNumberSection(15).should.equal("fifteen");
   });
   it("takes the edge case of input 10 and outputs its word value", function(){
-    numericalValue(10).should.equal("ten");
+    firstNumberSection(10).should.equal("ten");
   });
-
+  it("takes a number > 100 and returns its word form.", function(){
+    firstNumberSection(110).should.equal("one hundred and ten");
+  });
+  it("takes the number 100 and returns its word",function(){
+    firstNumberSection(100).should.equal("one hundred");
+  });
 });
+
+// describe("biginNumbas",function(){
+// })
