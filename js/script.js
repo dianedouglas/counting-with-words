28ef475bec6a1e3968aa8dd9cc79f909cf9e7b3f
dianedouglas & Lee Kebler <student@epicodus.com>
2014-07-14 14:37:07 -0700

    refactored the first two tests to use an array for single digit words and double digit words.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 var numericalValue = function(inputNumber){
-
-
+  inputNumber = inputNumber.toString().split("");
   var singleDigitWords = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  var secondDigitWords = ["teen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" ]
   var outputWord = "";
-  if(inputNumber < 10 ){
-    outputWord = singleDigitWords[inputNumber];
+
+  if (inputNumber.length === 1){
+    outputWord = singleDigitWords[inputNumber[0]];
+  } else if (inputNumber.length === 2){
+    outputWord = secondDigitWords[inputNumber[0]-1] + " " + singleDigitWords[inputNumber[1]];
   }
-  else if ((inputNumber > 20) && (inputNumber < 30)){
-    outputWord = "twenty " + singleDigitWords[inputNumber - 20];
-  }
+
+  // else if ((inputNumber > 20) && (inputNumber < 30)){
+  //   outputWord = "twenty " + singleDigitWords[inputNumber - 20];
+  // }
 
 
 
--- specs/specs.js
+++ specs/specs.js
@@ -5,4 +5,11 @@ describe("numericalValue", function(){
   it("takes a number in the 20's and returns its word version.", function(){
     numericalValue(25).should.equal("twenty five");
   });
+  it("takes a number in the 30's and returns its word version.", function(){
+    numericalValue(39).should.equal("thirty nine");
+  });
+  it("takes a multiple of 10 and returns its word version without a zero on the end.", function(){
+    numericalValue(20).should.equal("twenty");
+  });
+
 });
